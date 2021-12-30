f8a734b5dcdaf47d7423b43fdeb53df98eeb0388
Pranata <88884863+verdudev@users.noreply.github.com>
2021-12-30 14:20:35 +0700

    Update start.py

--- Userbot/start.py
+++ Userbot/start.py
@@ -1,7 +1,3 @@
-from datetime import datetime
-from sys import version_info
-from time import time
-
 from config import (
     BOT_NAME,
     BOT_USERNAME,
@@ -9,45 +5,14 @@ from config import (
     OWNER_NAME,
     UPDATES_CHANNEL,
 )
-from program import __version__
-from driver.filters import command, other_filters
-from driver.fsub import forcesubs
+from Userbot.helpers.fsub import forcesubs
+from config import HNDLR, bot, call_py
 from pyrogram import Client, filters
-from pyrogram import __version__ as pyrover
-from pytgcalls import (__version__ as pytover)
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
-
-__major__ = 0
-__minor__ = 2
-__micro__ = 1
-
-__python_version__ = f"{version_info[0]}.{version_info[1]}.{version_info[2]}"
-
-
-START_TIME = datetime.utcnow()
-START_TIME_ISO = START_TIME.replace(microsecond=0).isoformat()
-TIME_DURATION_UNITS = (
-    ("week", 60 * 60 * 24 * 7),
-    ("day", 60 * 60 * 24),
-    ("hour", 60 * 60),
-    ("min", 60),
-    ("sec", 1),
-)
-
-
-async def _human_time_duration(seconds):
-    if seconds == 0:
-        return "inf"
-    parts = []
-    for unit, div in TIME_DURATION_UNITS:
-        amount, seconds = divmod(int(seconds), div)
-        if amount > 0:
-            parts.append("{} {}{}".format(amount, unit, "" if amount == 1 else "s"))
-    return ", ".join(parts)
 
 
 @Client.on_message(
-    command(["start", f"start@{BOT_USERNAME}"]) & filters.private & ~filters.edited)
+    filters.command(["start"], prefixes=f"{HNDLR}"))
 @forcesubs
 async def start_(client: Client, message: Message):
     await message.reply_text(
