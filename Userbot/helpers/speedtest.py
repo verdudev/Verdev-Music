203fbbd74bcff51b9621def2006c4bd9a938e6f3
Pranata <88884863+verdudev@users.noreply.github.com>
2021-12-30 21:11:28 +0700

    Update speedtest.py

--- Userbot/helpers/speedtest.py
+++ Userbot/helpers/speedtest.py
@@ -20,7 +20,7 @@ def bytes(size: float) -> str:
 
 
 @Client.on_message(filters.command(["speedtest"], prefixes=f"{HNDLR}"))
-async def statsguwid(_, m: message):
+async def statsguwid(_, message):
     await m.delete()
     m = await message.reply_text("Menjalankan Tes Kecepatan ⚡")
     try:
